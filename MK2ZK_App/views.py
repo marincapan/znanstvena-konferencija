eab9256edf09b9d9df3188cff0d46aff74f18499
FranMesic <fran.mesic@fer.hr>
2021-11-02 23:05:33 +0100

    Dodana POST metoda na signup - Trenutačno ne sprema u bazu podataka.

--- MK2ZK_App/views.py
+++ MK2ZK_App/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 
 
 # Create your views here.
@@ -8,6 +9,20 @@ def home(request):
     return render(request, 'Index.html')
 
 def signup(request):
+    if request.method == "POST":
+        Username = request.POST['Username']
+        Fname = request.POST['Fname']
+        Lname = request.POST['Lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        #Ubaci u bazu podataka
+
+        #messages.success(request, "Success")
+
+        return redirect('signin')
+
     return render(request, 'Signup.html')
     
 def signin(request):
