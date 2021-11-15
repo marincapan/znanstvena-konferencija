e3ffd74d435140975b5056d93e1e6615acd7e5a1
Petra Dunja <petra.dunja.grujic.ostojic@estudent.hr>
2021-11-15 13:41:34 +0100

    prepravljen sql baze podataka

--- IzvorniKod/baza.sql
+++ IzvorniKod/baza.sql
@@ -1,101 +1,144 @@
-CREATE TABLE Sekcija
-(
-  sifSekcija INT,
-  nazivSekcija VARCHAR(50),
-  PRIMARY KEY (sifSekcija)
-);
-
 CREATE TABLE Ustanova
 (
-  sifUstanova INT,
-  nazivUstanova VARCHAR(50),
-  grad VARCHAR(50),
-  drzava VARCHAR(50),
-  adresa VARCHAR(50),
-  PRIMARY KEY (sifUstanova)
-);
-
-CREATE TABLE Autor
-(
-  sifAutor INT,
-  ime VARCHAR(50),
-  prezime VARCHAR(50),
-  email VARCHAR(50),
-  naznakaOZK INT,
-  PRIMARY KEY (sifAutor),
-  UNIQUE (email)
-);
-
-CREATE TABLE PoljaObrasca
-(
   ID INT,
-  naziv VARCHAR(50),
-  tipolja VARCHAR(50),
-  obavezno boolean,
+  naziv VARCHAR,
+  grad VARCHAR,
+  drzava VARCHAR,
+  adresa VARCHAR,
   PRIMARY KEY (ID)
-);
-
-CREATE TABLE Korisnik
-(
-  korisnickoIme VARCHAR(50),
-  lozinka VARCHAR(50),
-  ime VARCHAR(50),
-  prezime VARCHAR(50),
-  email VARCHAR(50),
-  idSudionik INT ,
-  odobren boolean,
-  vrstaKorisnik INT,
-  sifUstanova INT,
-  sifSekcija INT,
-  PRIMARY KEY (korisnickoIme),
-  FOREIGN KEY (sifUstanova) REFERENCES Ustanova(sifUstanova),
-  FOREIGN KEY (sifSekcija) REFERENCES Sekcija(sifSekcija),
-  UNIQUE (email),
-  UNIQUE (idSudionik)
-);
-
-CREATE TABLE Rad
-(
-  sifRad INT,
-  naslov VARCHAR(50),
-  ocjena INT,
-  recenziran boolean,
-  obrazlozenje VARCHAR(500),
-  recenzent VARCHAR(50),
-  PRIMARY KEY (sifRad),
-  FOREIGN KEY (recenzent) REFERENCES Korisnik(korisnickoIme)
-);
-
-CREATE TABLE PrijavaRada
-(
-  
-  sifSekcija INT,
-  sifRad INT,
-  korisnickoIme VARCHAR(50),
-  PRIMARY KEY (korisnickoIme, sifRad),
-  FOREIGN KEY (korisnickoIme) REFERENCES Korisnik(korisnickoIme),
-  FOREIGN KEY (sifRad) REFERENCES Rad(sifRad),
-  FOREIGN KEY (sifSekcija) REFERENCES Sekcija(sifSekcija),
-  UNIQUE (sifRad, sifSekcija)
 );
 
 CREATE TABLE Konferencija
 (
-  sifKonferencija INT,
-  nazivKonferencija VARCHAR(50) ,
-  opisKonferencija VARCHAR(1000),
-  datumKonferencija DATE ,
-  rokPrijava DATE ,
-  predsjedavajuci VARCHAR(50) ,
-  PRIMARY KEY (sifKonferencija),
-  FOREIGN KEY (predsjedavajuci) REFERENCES Korisnik(korisnickoIme)
+  ID INT,
+  naziv VARCHAR,
+  opis VARCHAR,
+  datum DATE,
+  rokPrijava TIMESTAMP,
+  rokAdmin TIMESTAMP,
+  rokRecenzent TIMESTAMP,
+  
+  PRIMARY KEY (ID)
+  
 );
+
+CREATE TABLE Sekcija
+(
+  ID INT,
+  naziv VARCHAR,
+  sifKonferencija INT,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (sifKonferencija) REFERENCES Konferencija(ID)
+);
+CREATE TABLE Autor
+(
+  ID INT,
+  ime VARCHAR,
+  prezime VARCHAR,
+  email VARCHAR,
+  naznakaOZK BOOLEAN ,
+  PRIMARY KEY (ID),
+  UNIQUE (email)
+);
+CREATE TABLE TipPoljeObrasca
+(
+  ID INT,
+  naziv VARCHAR,
+  PRIMARY KEY (ID)
+);
+CREATE TABLE DodatnoPoljeObrasca
+(
+  ID INT,
+  ime VARCHAR,
+  tipPolja INT,
+  obavezno BOOLEAN,
+  prisutno BOOLEAN,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (tipPolja) REFERENCES TipPoljeObrasca(ID)
+);
+
+
+
+CREATE TABLE Uloga
+(
+  ID INT,
+  naziv VARCHAR,
+  PRIMARY KEY (ID)
+);
+CREATE TABLE Korisnik
+(
+  ID INT,
+  korisnickoIme VARCHAR,
+  lozinka VARCHAR,
+  ime VARCHAR,
+  prezime VARCHAR,
+  email VARCHAR,
+  idSudionik INT,
+  odobren BOOLEAN,
+  token VARCHAR,
+  potvrdioPrijava BOOLEAN,
+  sifUstanova INT,
+  sifSekcija INT,
+  ulogaKorisnik INT,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (sifUstanova) REFERENCES Ustanova(ID),
+  FOREIGN KEY (sifSekcija) REFERENCES Sekcija(ID),
+  FOREIGN KEY (ulogaKorisnik) REFERENCES Uloga(ID),
+  UNIQUE (email),
+  UNIQUE (idSudionik),
+  UNIQUE (korisnickoIme)
+);
+CREATE TABLE Ocjena
+(
+  ID INT,
+  znacenje VARCHAR,
+  PRIMARY KEY (ID)
+);
+
+
+CREATE TABLE Rad
+(
+  ID INT,
+  naslovRad VARCHAR,
+  pdf VARCHAR UNIQUE,
+  sifSekcija INT,
+  prijavioID INT,
+  recenziran BOOLEAN,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (sifSekcija) REFERENCES Sekcija(ID),
+  FOREIGN KEY (prijavioID) REFERENCES Korisnik(ID)
+);
+
+
+
 
 CREATE TABLE AutorRad
 (
-  sifRad INT ,
+  sifRad INT,
   sifAutor INT,
   PRIMARY KEY (sifRad, sifAutor),
-  FOREIGN KEY (sifRad) REFERENCES Rad(sifRad),
-  FOREIGN KEY (sifAutor) REFERENCES Autor(sifAutor)
+  FOREIGN KEY (sifRad) REFERENCES Rad(ID),
+  FOREIGN KEY (sifAutor) REFERENCES Autor(ID)
+  
+);
+CREATE TABLE DodatniPodatak
+(
+  korisnikID INT,
+  poljeObrascaID INT,
+  PRIMARY KEY (korisnikID, poljeObrascaID),
+  FOREIGN KEY (korisnikID) REFERENCES Korisnik(ID),
+  FOREIGN KEY (poljeObrascaID) REFERENCES DodatnoPoljeObrasca(ID)
+  
+);
+
+CREATE TABLE Recenzija
+(
+  ID INT,
+  ocjena INT,
+  obrazlozenje VARCHAR,
+  sifRad INT,
+  recenzentID INT,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (recenzentID) REFERENCES Korisnik(ID),
+  FOREIGN KEY (sifRad) REFERENCES Rad(ID)
 );
