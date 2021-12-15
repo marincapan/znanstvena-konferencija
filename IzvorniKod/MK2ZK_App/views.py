f108b885aee80c9a3af91f75e522b166b2619720
Luka Mujagic <luka.mujagic@fer.hr>
2021-12-15 16:05:01 +0100

    Uredjen izgled pocetne stranice za pregled
    Izgled je popravljen. Stavljena je i porukica koja objasnjava
    sto predstavljaju brojevi kraj svake opcije, to bi se eventualno
    moglo pretvoriti u neki "?" ili tooltip.

--- IzvorniKod/MK2ZK_App/views.py
+++ IzvorniKod/MK2ZK_App/views.py
@@ -470,6 +470,18 @@ def pregled(request):
     
     if "LoggedInUserRole" in request.session:
         context["LoggedInUserRole"]=request.session['LoggedInUserRole']
+
+    recenzenti = models.Korisnik.objects.filter(vrstaKorisnik_id=3)
+    sudionici = models.Korisnik.objects.filter(vrstaKorisnik_id=1)
+    radovi = models.Rad.objects.all()
+
+    context["recenzenti_broj_odobrenih"] = recenzenti.filter(odobrenBool=True).count()
+    context["recenzenti_broj_neodobrenih"] = recenzenti.filter(odobrenBool=False).count()
+    context["sudionici_broj_odobrenih"] = sudionici.filter(odobrenBool=True).count()
+    context["sudionici_broj_neodobrenih"] = sudionici.filter(odobrenBool=False).count()
+    context["radovi_broj_recenziranih"] = radovi.filter(recenziranBool=True).count()
+    context["radovi_broj_nerecenziranih"] = radovi.filter(recenziranBool=False).count()
+
     return render(request, 'Pregled.html', context)
 
 def recenzenti(request):
@@ -484,7 +496,7 @@ def recenzenti(request):
     sekcije = models.Sekcija.objects.all()
     ustanove = models.Ustanova.objects.all()
 
-    """Shvatio sam da je puno lakse napravit ovo nego stavljat naziv unutar templatea"""
+    #Shvatio sam da je puno lakse napravit ovo nego stavljat naziv unutar templatea
     for recenzent in recenzenti:
         recenzent.korisnikSekcija_naziv = sekcije.get(sifSekcija=recenzent.korisnikSekcija_id).naziv
         recenzent.korisnikUstanova_naziv = ustanove.get(sifUstanova=recenzent.korisnikUstanova_id).naziv
@@ -506,7 +518,7 @@ def sudionici(request):
     sekcije = models.Sekcija.objects.all()
     ustanove = models.Ustanova.objects.all()
 
-    """Shvatio sam da je puno lakse napravit ovo nego stavljat naziv unutar templatea"""
+    #Shvatio sam da je puno lakse napravit ovo nego stavljat naziv unutar templatea
     for sudionik in sudionici:
         sudionik.korisnikSekcija_naziv = sekcije.get(sifSekcija=sudionik.korisnikSekcija_id).naziv
         sudionik.korisnikUstanova_naziv = ustanove.get(sifUstanova=sudionik.korisnikUstanova_id).naziv
@@ -516,15 +528,17 @@ def sudionici(request):
     return render(request, 'Sudionici.html', context)
 
 def radovi(request):
-    print(request.method)
     if request.method == "POST":
+        #Dohvacanje svih potrebnih podataka
         korisnici = models.Korisnik.objects.all()
         radovi = models.Rad.objects.all()
 
+        #Postavljanje zipa
         zip_name = "radovi.zip"
         s = BytesIO()
         zip = zipfile.ZipFile(s, "w")
 
+        #Iteriranje kroz sve radove kako bi se mogla napraviti odgovarajuca struktura .zip datoteke
         for rad in radovi:
             if(rad.pdf.name == ''):
                 break
@@ -551,7 +565,7 @@ def radovi(request):
     sekcije = models.Sekcija.objects.all()
     korisnici = models.Korisnik.objects.all()
 
-    """Shvatio sam da je puno lakse napravit ovo nego stavljat naziv unutar templatea"""
+    #Shvatio sam da je puno lakse napravit ovo nego stavljat naziv unutar templatea
     for rad in radovi:
         rad.radSekcija_naziv = sekcije.get(sifSekcija=rad.radSekcija_id).naziv
         rad.radKorisnik_prezime = korisnici.get(id=rad.radKorisnik_id).prezime
