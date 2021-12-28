adba6130f46f3e9eaebaf2acf812be929cd66ebc
FranMesic <fran.mesic@fer.hr>
2021-12-28 12:37:50 +0100

    Popravljena funkcionalnost stranice 'covidstats'

--- IzvorniKod/MK2ZK_App/views/adminView.py
+++ IzvorniKod/MK2ZK_App/views/adminView.py
@@ -388,13 +388,30 @@ def covidstats(request):
     data=bytes.decode('UTF-8')
     data=data.split("\n")
     fetchUstanove = models.Ustanova.objects.all()
+
+    module_dir = os.path.dirname(__file__)  # get current directory
+    path1 = os.path.join(module_dir, '../static/txt/SveDrzave.txt')
+    path2 = os.path.join(module_dir, '../static/txt/SveDrzaveHR.txt')
+
+    file1 = open(path1, "r", encoding='utf-8')
+    file2 = open(path2, "r", encoding='utf-8')
+
+    drzaveEng = file1.read().replace("\"","\'")
+    drzaveHrv = file2.read().replace("\"","\'")
+
+    drzaveEngList = drzaveEng[1:len(drzaveEng)-2].split("',\n '")
+    drzaveHrvList = drzaveHrv[1:len(drzaveHrv)-2].split("',\n '")
+
     konfDrzave={}
     for row in data:
         date=row.split(",") #0 - Date_reported,1 - Country_code,2 - Country,3 - WHO_region,4 - New_cases,5 - Cumulative_cases,6 - New_deaths, 7- Cumulative_deaths
         for ustanova in fetchUstanove:
-            if ustanova.drzava in date:
-                if date[0]!="": #Kraj
-                    konfDrzave[str(date[2])]=[date[0],date[4]] # Rijecnik s drzavom (Na engleskom zasad) i vrijednostima koje zelim prenijeti (zasad datum dohavacanja podataka i novih slucajeva)
+            if ustanova.drzava in drzaveHrvList:
+                indexHRV=drzaveHrvList.index(ustanova.drzava)
+                drzavaENG=drzaveEngList[indexHRV]
+                if drzavaENG in date:
+                    if date[0]!="": #Kraj
+                        konfDrzave[str(ustanova.drzava)]=[date[0],date[4]] # Rijecnik s drzavom (Na engleskom zasad) i vrijednostima koje zelim prenijeti (zasad datum dohavacanja podataka i novih slucajeva)
     context["konfDrzave"]=konfDrzave
     print(context)    
     return render(request, 'CovidStats.html', context)
