f2becb5ceea147aa17ab6dc471560375bfc932e6
Marin Capan <marin.capan@fer.hr>
2022-01-02 17:28:01 +0100

    dodana provjera statičnih datoteka

--- IzvorniKod/docker-entrypoint.sh
+++ IzvorniKod/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python manage.py flush --no-input
+python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata initial_MK2ZK_data.json
